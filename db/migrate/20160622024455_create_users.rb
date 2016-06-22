df51ad1b4384d32da9cfdf5a94319770417cf305
phanbt58 <phanbt95@gmail.com>
2016-06-22 10:33:14 +0700

    Sign up

--- db/migrate/20160622024455_create_users.rb
+++ db/migrate/20160622024455_create_users.rb
@@ -4,10 +4,13 @@ class CreateUsers < ActiveRecord::Migration
       t.string :name
       t.string :email
       t.string :password
+      t.string :password_confirmation
       t.string :avatar
       t.boolean :is_admin
 
       t.timestamps null: false
     end
+    add_index :users, :email, unique: true
+    add_column :users, :password_digest, :string
   end
 end
