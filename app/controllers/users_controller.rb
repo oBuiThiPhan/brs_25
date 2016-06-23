a8e0b1f69b66d3ad933d870a0fb7fcac1d22ea09
phanbt58 <phanbt95@gmail.com>
2016-06-23 12:56:35 +0700

    User edit profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:index, :edit, :update]
+  before_action :correct_user, only: [:edit, :update]
+
   def new
     @user = User.new
   end
@@ -17,8 +20,28 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = User.find_by id: params[:id]
+  end
+
+  def update
+    @user = User.find_by id: params[:id]
+    if @user.update_attributes user_params
+      flash[:success] = t "users.update.succee"
+      redirect_to @user
+    else
+      render :edit
+    end
+  end
+
   private
   def user_params
     params.require(:user).permit :name, :email, :password, :password_confirmation, :avatar
   end
+
+  def correct_user
+    @user = User.find_by id: params[:id]
+    redirect_to root_url unless @user.current_user? current_user
+  end
+
 end
