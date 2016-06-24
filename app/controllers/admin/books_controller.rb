70add180ebb383888fd653c2fdfbd05bf24549bd
nts <ngotungson0602@gmail.com>
2016-06-24 10:06:33 +0700

    Add edit book function for admin

--- app/controllers/admin/books_controller.rb
+++ app/controllers/admin/books_controller.rb
@@ -1,5 +1,6 @@
 class Admin::BooksController < ApplicationController
   before_action :logged_as_admin, only: [:index, :new, :create]
+  before_action :get_categories, except: [:index]
 
   def index
     @books = Book.all
@@ -7,24 +8,38 @@ class Admin::BooksController < ApplicationController
 
   def new
     @book = Book.new
-    @categories = Category.all
   end
 
   def create
     @book = Book.new book_params
     if @book.save
-      flash[:success] = t "controllers.admin.books.create.flash.success"
+      flash[:success] = t "controllers.admin.book.create.flash.success"
       redirect_to admin_books_url
     else
-      @categories = Category.all
       render :new
+    end
+  end
+
+  def edit
+    @book = Book.find_by id: params[:id]
+  end
+
+  def update
+    @book = Book.find_by id: params[:id]
+    if @book.update_attributes book_params
+      redirect_to admin_books_url
+    else
+      render :edit
     end
   end
 
   private
   def book_params
-    params.require(:book).permit :category, :title, :author,
+    params.require(:book).permit :category_id, :title, :author,
       :number_of_pages, :publish_date, :image
   end
 
+  def get_categories
+    @categories = Category.all
+  end
 end
