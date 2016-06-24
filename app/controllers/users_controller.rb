fa91264288a21465743301f8a9ed5d0ed02950eb
phanbt58 <phanbt95@gmail.com>
2016-06-24 11:36:47 +0700

    Show a user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,11 @@ class UsersController < ApplicationController
   end
 
   def show
+    @user = User.find_by id: params[:id]
+    if @user.nil?
+      flash[:warning] = t "views.users.show.nouser"
+      redirect_to root_url
+    end
   end
 
   def create
