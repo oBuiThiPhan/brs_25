56ba29625ac2ee8fcee8b2d2f5a0df9063ab800d
nts <ngotungson0602@gmail.com>
2016-06-28 13:28:29 +0700

    add delete-request function for admin

--- app/controllers/admin/requests_controller.rb
+++ app/controllers/admin/requests_controller.rb
@@ -1,8 +1,25 @@
 class Admin::RequestsController < ApplicationController
   before_action :logged_as_admin
+  before_action :load_request, only: [:destroy]
 
   def index
-    @requests = Request.order("id desc")
+    @requests = Request.order("created_at desc")
       .paginate page: params[:page], per_page: Settings.per_page
+  end
+
+  def destroy
+    if @request && @request.destroy
+      flash[:success] = t "controllers.flash.common.destroy_success",
+        objects: t("activerecord.model.request")
+    else
+      flash[:danger] = t "controllers.flash.common.destroy_error",
+        objects: t("activerecord.model.request")
+    end
+    redirect_to admin_requests_url
+  end
+
+  private
+  def load_request
+    @request = Request.find_by id: params[:id]
   end
 end
